16bf3172382b42fbef78d8bffa6b2a16d80f2d77
psalv <WalrusMango1>
2017-02-15 18:01:23 -0500

    Altered comments.

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -24,29 +24,39 @@ public class MinHeap {
         // no initialization data.
     }
 
+    // Takes O(nlgn) time.
     public MinHeap(HasKey_ data[]){
         for(HasKey_ i: data){
             insert(i);
         }
     }
 
-    // Takes O(nlgn) time.
+    // Takes O(lgn) time.
     private void insert(HasKey_ data){
         heap.add(data);
         buildMinHeapify(heap.size() - 1);
     }
 
-     // Takes O(lgn) time since we can look at each parent until we reach the root.
-     private void buildMinHeapify(int i){
+    // Takes O(lgn) time since we can look at each parent until we reach the root.
+    private void buildMinHeapify(int i){
         if(i == 0){
             return;
         }
+
+        // Determining the parent.
         int adjust = i % 2 == 0 ? 1 : 0;
         int parent = (int)Math.floor(i/2.0) - adjust;
+
+        // Since we assume that the current min heap maintains the min heap property,
+        // we need only look at the parent.
+        // If the new node is less than the parent, than it is also less than any of the children's parents.
+        // We can therefore swap the parent and the new node.
         if(heap.get(parent).getKey() > heap.get(i).getKey()){
             HasKey_ temp = heap.get(parent);
             heap.set(parent, heap.get(i));
             heap.set(i, temp);
+
+            // We do this until we reach the root, or until our node is in the correct place (parent is smaller).
             buildMinHeapify(parent);
         }
     }
@@ -61,9 +71,11 @@ public class MinHeap {
             return;
         }
 
+        // Need to determine the smallest element between i and it's two children
         int smallest = heap.get(left).getKey() < heap.get(i).getKey() ? left : i;
         smallest = heap.get(left).getKey() < heap.get(smallest).getKey() ? right : smallest;
 
+        // If the children are smaller we need to swap and recurse upwards.
         if(smallest != i){
             HasKey_ temp = heap.get(smallest);
             heap.set(smallest, heap.get(i));
@@ -73,10 +85,11 @@ public class MinHeap {
         }
     }
 
-    // To extract the min it takes O(lgn) time since we need to minheapify the root.
+    // To extract the min it takes O(lgn) time since we need to min-heapify the root.
     private HasKey_ extractMin(){
         int last = heap.size() - 1;
 
+        // Swap with min with the end, remove the end, and min-heapify the root.
         HasKey_ min = heap.get(0);
         heap.set(0, heap.get(last));
         heap.set(last, min);
