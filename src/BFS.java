65e233dd96d6206ce237d5956d1a2fe00ff9af15
psalv <psalvato@uwo.ca>
2017-04-09 15:27:54 -0400

    tested bfs

--- src/BFS.java
+++ src/BFS.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 
 public class BFS {
 
+
     public static boolean solveBFS(GraphNode root, String endKey){
         LinkedList<GraphNode> queue = new LinkedList<>();
         HashSet<GraphNode> seen = new HashSet<>();
@@ -13,6 +14,7 @@ public class BFS {
             if(cur.getKey().equals(endKey)){
                 return true;
             }
+
             for(GraphEdge e: cur.getAdjList()){
                 GraphNode temp = e.getOpposite(cur);
                 if(!seen.contains(temp)){
@@ -21,16 +23,36 @@ public class BFS {
                 }
             }
         }
-        return false;
 
+        return false;
     }
 
     public static void main(String[] args) {
+
+        // We are building a digraph, if we wanted an undigraph we would add the edge to each adj list.
         GraphNode a = new GraphNode("a");
         GraphNode b = new GraphNode("b");
         GraphNode c = new GraphNode("c");
         GraphNode d = new GraphNode("d");
         GraphNode e = new GraphNode("e");
         GraphNode f = new GraphNode("f");
+        GraphEdge e1 = new GraphEdge(a, b);
+        a.addEdge(e1);
+        GraphEdge e2 = new GraphEdge(a, c);
+        a.addEdge(e2);
+        GraphEdge e3 = new GraphEdge(b, a);
+        b.addEdge(e3);
+        GraphEdge e8 = new GraphEdge(b, d);
+        b.addEdge(e8);
+        GraphEdge e4 = new GraphEdge(c, f);
+        c.addEdge(e4);
+        GraphEdge e5 = new GraphEdge(c, c);
+        c.addEdge(e5);
+        GraphEdge e6 = new GraphEdge(f, e);
+        f.addEdge(e6);
+        GraphEdge e7 = new GraphEdge(e, a);
+        e.addEdge(e7);
+
+        System.out.println(solveBFS(a, "c"));
     }
 }
